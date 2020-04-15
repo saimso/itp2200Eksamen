cc3550f4de0a3efeba31bd30ff3ce8904b5225f2
j-chug <johanchg@gmail.com>
2020-04-15 18:48:34 +0200

    Added a suggestion to how the Exercise class will look

--- ITP2200ExamTest/src/HomeExerciseProgram/Exercise.java
+++ ITP2200ExamTest/src/HomeExerciseProgram/Exercise.java
@@ -1,4 +1,18 @@
 package HomeExerciseProgram;
 
 public abstract class Exercise {
+    public int intensity; // Thinking we can use a 1-10 scale for intensity where 10 is the highest
+    public int duration; //total minutues for whole exercise
+    public int repetitions; // Number of reps in each set
+    public int sets; // Number of sets
+
+    public void setIntensity(int intensity){this.intensity = intensity;}
+    public void setDuration(int minutes){this.duration = minutes;}
+    public void setRepetitions(int reps){this.repetitions = reps;}
+    public void setSets(int sets){this.sets = sets;}
+
+    public int getIntensity(){return intensity;}
+    public int getDuration(){return duration;}
+    public int getRepetitions(){return repetitions;}
+    public int getSets(){return sets;}
 }
