1a82b553ceb24e93f337d77da87d7fc6e4b4623f
j-chug <johanchg@gmail.com>
2020-04-15 19:18:21 +0200

    Added some code to the StrengthExercise class

--- ITP2200ExamTest/src/HomeExerciseProgram/StrengthExercise.java
+++ ITP2200ExamTest/src/HomeExerciseProgram/StrengthExercise.java
@@ -1,4 +1,21 @@
 package HomeExerciseProgram;
 
 public class StrengthExercise extends Exercise {
+    public int weights;
+    public String equipment;
+    public StrengthExercise(int intensity, int duration, int repetitions, int sets, int weights, String equipment){
+        super.setIntensity(intensity);
+        super.setDuration(duration);
+        super.setRepetitions(repetitions);
+        super.setSets(sets);
+        setWeights(weights);
+        setEquipment(equipment);
+    }
+
+    public void setWeights(int weight){
+        this.weights = weight;
+    }
+    public void setEquipment(String equipment){
+        this.equipment = equipment;
+    }
 }
