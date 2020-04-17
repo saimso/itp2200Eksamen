e13dbfc7ba67d711c54d7b5d834d2fc126ca02ea
j-chug <johanchg@gmail.com>
2020-04-17 07:20:41 +0200

    added an override toString method and a dummy-object to test code

--- ITP2200ExamTest/src/HomeExerciseProgram/ExerciseManager.java
+++ ITP2200ExamTest/src/HomeExerciseProgram/ExerciseManager.java
@@ -1,7 +1,8 @@
 package HomeExerciseProgram;
 
 public class ExerciseManager {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) { //La til en prøvetekst for å teste kode
+        StrengthExercise s1 = new StrengthExercise(8,10,10,3,30,"Barbell","Lift the barbell from shoulder-height and up");
+        System.out.println(s1.toString());
     }
 }
--- ITP2200ExamTest/src/HomeExerciseProgram/StrengthExercise.java
+++ ITP2200ExamTest/src/HomeExerciseProgram/StrengthExercise.java
@@ -1,21 +1,31 @@
 package HomeExerciseProgram;
 
 public class StrengthExercise extends Exercise {
-    public int weights;
-    public String equipment;
-    public StrengthExercise(int intensity, int duration, int repetitions, int sets, int weights, String equipment){
+    public double weights; // Number of kilograms to use (changed to double due to possible decimal usage)
+    public String equipment; // What equipment do you need
+    public String operation; // For describing the exercise
+
+    public StrengthExercise(int intensity, int duration, int repetitions, int sets, int weights, String equipment, String operation){
         super.setIntensity(intensity);
         super.setDuration(duration);
         super.setRepetitions(repetitions);
         super.setSets(sets);
         setWeights(weights);
         setEquipment(equipment);
+        setOperation(operation);
     }
 
-    public void setWeights(int weight){
+    public void setWeights(double weight){
         this.weights = weight;
     }
     public void setEquipment(String equipment){
         this.equipment = equipment;
+    }
+    public void setOperation(String operation){this.operation = operation;}
+
+    @Override
+    public String toString(){ //Overrider toString for å printe ut objekt som ønsket
+        return "Intensity: " + intensity + "\nDuration: " + duration + "\nRepetitions: " + repetitions + "\nSets: " + sets + "\nWeight to lift: " + weights +
+                "\nEquipment: " + equipment + "\nOperation: " + operation;
     }
 }
